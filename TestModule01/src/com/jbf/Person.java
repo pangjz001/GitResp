398c8902135f95d4c1dc68250c876e693ca0c943
zhou <pangjz001@126.com>
2022-03-31 14:57:28 +0800

    增加了person

---
+++ TestModule01/src/com/jbf/Person.java
@@ -0,0 +1,12 @@
+package com.jbf;
+
+/**
+ * @Auther: zhou
+ * @Date: 2022/3/31 - 03 - 31 - 14:55
+ * @Description: com.jbf
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+    private String name;
+}
